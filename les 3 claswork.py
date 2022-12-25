13083381656569ca35200ab4717a65e8a47fb82c
Andreas-Lych <120594955+Andreas-Lych@users.noreply.github.com>
2022-12-26 01:54:07 +0300

    Add files via upload

---
+++ les 3 claswork.py
@@ -0,0 +1,88 @@
+x = 17 / 2 * 3 + 2
+
+print(x)
+
+x = 2 + 17 / 2 * 3
+
+print(x)
+
+x = 19 % 4 + 15 / 2 * 3
+
+print(x)
+
+x = (15 + 6) - 10 * 4
+
+print(x)
+
+x = 17 / 2 % 2 * 3
+
+print(x)
+
+x = 17 / 2 * (3 + 2)
+
+print(x)
+
+x = (2 + 17) / 2 * 3
+
+print(x)
+
+x = 19 % (4 + 15) / 2 * 3
+
+print(x)
+
+x = 15 + (6 - 10) * 4
+
+print(x)
+
+x = 17 / 2 % (2 * 3)
+
+print(x)
+
+ruslan = 35
+
+evgeny = 41
+
+oleg = 40
+
+x = oleg + evgeny + ruslan
+
+print(x)
+
+ruslan = 35
+
+evgeny = 41
+
+oleg = 40
+
+av_age = (oleg + evgeny + ruslan) / 3
+
+print(av_age)
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+my_list[-5:-7]
+
+from math import sqrt
+
+x = 6
+
+my_list = ['p', 's', 'd']
+
+p = x * x
+print(p)
+
+s = x + x + x + x
+print(s)
+
+d = (sqrt(2)) * x
+print(d)
+
+
+
+
+
+
+
+
+
+
