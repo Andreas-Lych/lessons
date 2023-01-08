24e5c8778e5c29e2bc0801decb8cea7fbf759cf9
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-08 14:53:53 +0300

    отправить

--- lesson_04/clas 1.py
+++ lesson_04/clas 1.py
@@ -1,22 +1,22 @@
 import random
 
-my_lyst =[]
+my_list =[]
 
 x = random.randint(0, 20)
 for i in range(x):
     y = random.randint(0, 100)
-    my_lyst.append(y)
+    my_list.append(y)
 
-print(my_lyst)
+print(my_list)
 
 for i in range(3):
     y = random.randint(0, 100)
-    my_lyst.append(y)
+    my_list.append(y)
 
-print(my_lyst)
+print(my_list)
 
 result = 0
 
-for element in my_lyst:
+for element in my_list:
     if element > 10:
         result = result + element
