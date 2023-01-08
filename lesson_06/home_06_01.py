3fb3f91ea60a6e92eb5f1a3450252b711c18fcaf
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-08 20:55:51 +0300

    отправить

--- lesson_06/home_06_01.py
+++ lesson_06/home_06_01.py
@@ -8,6 +8,21 @@ file_obj = open("LICENSE", "r")
 for line in file_obj:
    print(line)
 
+import collections
 
+text = line
+words = line.split()
+counter = collections.Counter(words)
+longest = max(words, key=len)
+
+print(longest)
+
+from collections import Counter
+import re
+
+text = line
+
+cnt = Counter(x for x in re.findall(r'[A-z\']{2,}', text))
+print(cnt.most_common(1))
 
 
