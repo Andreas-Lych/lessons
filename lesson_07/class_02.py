74f7e89515283c85366650ff4608decedd098b55
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-09 21:50:09 +0300

    отправить

--- lesson_07/class_02.py
+++ lesson_07/class_02.py
@@ -6,20 +6,22 @@
 
 import random
 from time import sleep
+from datetime import datetime
+
 def my_decorator(func):
     def silencer():
         start_time = datetime.now()
         func()
         end_time = datetime.now()
         print(end_time - start_time)
-        return silencer
+    return silencer
 
 
 @my_decorator
 def my_gun():
-    delay = random.randit(1, 5)
+    delay = random.randint(1, 5)
     sleep(delay)
 
 
-    if __name__ == "__main__":
-        my_gun()
+if __name__ == "__main__":
+    my_gun()
