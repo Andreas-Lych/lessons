163312ceb28f5cffe024ceefa038d5631c5788b0
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-11 18:03:59 +0300

    отправить

---
+++ lesson_07/home_01.py
@@ -0,0 +1,12 @@
+"""
+Дан список стран и городов каждой страны, где ключи это названия стран, а значения - списки городов в этих странах.
+Написать функцию которая осуществляет поиск по городу и возвращает страну. Оформить в виде программы,
+которая считывает название города и выводит на печать страну.
+"""
+
+my_dict = {"russia": "moscow", "russia": "rostov", "russia": "kazan", "russia": "smolensk", "belarus": "minsk",
+           "belarus": "brest", "belarus": "mogilev", "belarus": "gomel", "germany": "berlin", "germany": "bremen",
+           "germany": "bayern", "germany": "dortmund"}
+
+for value in my_dict
+    if value
