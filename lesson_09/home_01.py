4e1e0c991e73f01dd9c86c54d0f002c495329267
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-22 15:15:13 +0300

    отправить

--- lesson_09/home_01.py
+++ lesson_09/home_01.py
@@ -5,27 +5,55 @@ Triangle (атрибуты: три точки, методы: нахождени�
 Square (атрибуты: две точки, методы: нахождение площади и периметра).
 При потребности создавать все необходимые методы не описанные в задании.
 """
-Class Point:
-x = int(input())
-y = int(input())
+import math
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
 
-Class Figure:
+class Figure:
+    @staticmethod
+    def segment_length(a: Point, b: Point):
+        return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+    def perimeter(self):
+        raise NotImplementedError
+    def area(self):
+        raise NotImplementedError
 
-Class Circle(Figure):
-    def __init__(self, radius):
+class Circle(Figure):
+    def __init__(self, a: Point, radius: int):
+        self.a = a
         self.radius = radius
-def area(self):
-    return 3.14 * self.radius ** 2
+    def perimeter(self):
+        return round(2 * math.pi * self.radius, 2)
+    def area(self):
+        return round(math.pi * (self.radius ** 2), 2)
 
-Class Triangle(Figure):
-    def __init__(self, area):
-        self.area = area
-def area(self):
-    return (a*b)/2
-
-Class Square(Figure):
-    def __init__(self, length):
-        self.length = length
-def area(self):
-    return self.length ** 2
-
+class Triangle(Figure):
+    def __init__(self, a: Point, b: Point, c: Point):
+        self.a = a
+        self.b = b
+        self.c = c
+        # Calculate segments length using points
+        self.segment_1 = self.segment_length(self.a, self.b)
+        self.segment_2 = self.segment_length(self.b, self.c)
+        self.segment_3 = self.segment_length(self.c, self.a)
+    def perimeter(self):
+        return round(self.segment_1 + self.segment_2 + self.segment_3, 2)
+    def area(self):
+        # Use Heron's Formula to calculate triangle area
+        half_pr = self.perimeter() / 2
+        return round(math.sqrt(
+            half_pr * (half_pr - self.segment_1) * (half_pr - self.segment_2) * (half_pr - self.segment_3)
+        ), 2)
+class Square(Figure):
+    def __init__(self, a: Point, b: Point):
+        self.a = a
+        self.b = b
+        # Calculate segments length using points
+        self.segment_1 = abs(self.a.x - self.b.x)
+        self.segment_2 = abs(self.a.y - self.b.y)
+    def perimeter(self):
+        return round((self.segment_1 + self.segment_2) * 2, 2)
+    def area(self):
+        return round(self.segment_1 * self.segment_2, 2)
--- lesson_09/home_02.py
+++ lesson_09/home_02.py
@@ -1,6 +1,7 @@
 """
 Создать список фигур и в цикле подсчитать и вывести площади всех фигур на экран.
 """
+# мой метод - не правильный
 import math
 tip=str(input("Введите название фигуры ="))
 if tip=="треугольник":
@@ -17,3 +18,22 @@ elif tip=="круг":
     r=float(input("Введите радиус r ="))
     s=math.pi*(r**2)
 print(s)
+
+# from manti
+from lesson_09.home_01 import Point, Circle, Triangle, Square
+
+if __name__ == "__main__":
+    a = Point(3, 7)
+    circle = Circle(a, 3)
+
+    a = Point(3, 7)
+    b = Point(5, 2)
+    square = Square(a, b)
+
+    a = Point(1, 1)
+    b = Point(7, 2)
+    c = Point(5, 5)
+    triangle = Triangle(a, b, c)
+
+    for figure in [circle, square, triangle]:
+        print(figure.__class__.__name__, figure.perimeter(), figure.area())
