637946932627b3d8ba81879e62f92c47df08c7d9
Andreas-Lych <caesar.a.1981@gmail.com>
2023-01-22 14:26:03 +0300

    отправить

--- lesson_10/class_02.py
+++ lesson_10/class_02.py
@@ -2,8 +2,11 @@
 Создать тест в отдельном файле и проверить несколько телефонных номеров.
 """
 
-import re
-import logging
 
+from lesson_10.class_01 import check_string
+
+if __name__ == "__main__":
+    assert check_string("+375 (29) 299-20-29") is not None
+    assert check_string("+375 (29) 300-00-00") is not None
 
 
