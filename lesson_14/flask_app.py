04f0238d5caacb18ae3c5873f54efef6b6120c3e
Andreas-Lych <caesar.a.1981@gmail.com>
2023-02-08 21:48:06 +0300

    отправить

--- lesson_14/flask_app.py
+++ lesson_14/flask_app.py
@@ -1,13 +1,20 @@
+import logging
+
 from flask import Flask, request
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        logger.info("обрабатываем POST запрос")
         return request.form.to_dict()
-    return request.args.to_dict()
+
+    logger.info("обрабатываем GET запрос")
+    return request.args.to_dict() # через кавычки выводит текст в эксплорер
 
 
 if __name__ == "__main__":
