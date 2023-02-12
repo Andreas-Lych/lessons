12dc12c937dfca3e3c39a4f468f5b8d4a6e539b8
Andreas-Lych <caesar.a.1981@gmail.com>
2023-02-13 00:25:25 +0300

    отправить

--- lesson_13/main.py
+++ lesson_13/main.py
@@ -25,6 +25,30 @@ def search_users(session, email):
     session.find(user)
     return user
 
+def create_address(session, city, address):
+    address = Address(city=city, address=address)
+    session.add(address)
+    session.commit()
+    return address
+
+def create_product(session, name, price):
+    product = Product(name=name, price=price)
+    session.add(product)
+    session.commit()
+    return product
+
+def create_profile(session, phone, age):
+    profile = Profile(phone=phone, age=age)
+    session.add(profile)
+    session.commit()
+    return profile
+
+def create_purchase(session, user_id, product_id, count):
+    purchase = Purchase(user_id=user_id, product_id=product_id, count=count)
+    session.add(purchase)
+    session.commit()
+    return purchase
+
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -51,8 +75,14 @@ if __name__ == "__main__":
         if choice == "1":
             email = input ("введите email")
             password = input ("введите пароль")
+            city = input ("введите город")
+            address = input ("ввести адрес")
+            phone = input ("ввести телефон")
+            age = input ("ввести возраст")
 
             user = create_user(session, email, password)
+            address = create_address(session, city, address)
+            profile = create_profile(session, phone, age)
             logger.info(f"User #{user.id} is created")
 
         elif choice == "3":
